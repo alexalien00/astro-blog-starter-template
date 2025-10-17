44cfd4057c8fb0bc7aff0b9969d2329b45d26fd9
alexalien00 <alexandre.grondin.007@gmail.com>
2025-10-17 12:10:49 -0400

    Updated consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Alexandre Grondin - District 4";
+export const SITE_DESCRIPTION = "Ensemble, bâtissons l'avenir de Granby";
